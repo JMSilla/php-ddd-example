3673ec2fe73645e69409e30292cc10ae6b775ed0
JMSilla <josilma1@gmail.com>
2019-12-14 11:00:21 +0000

    Added new update video description command.

---
+++ src/Mooc/Videos/Application/UpdateDescription/UpdateVideoDescriptionCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+use CodelyTv\Shared\Domain\Bus\Command\Command;
+use CodelyTv\Shared\Domain\ValueObject\Uuid;
+
+final class UpdateVideoDescriptionCommand extends Command
+{
+    private $id;
+    private $description;
+
+    public function __construct(Uuid $commandId, string $id, string $description) {
+        parent::__construct($commandId);
+        
+        $this->id = $id;
+        $this->description = $description;
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function description(): string
+    {
+        return $this->description;
+    }
+}
+?>
